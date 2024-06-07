058da0d6277475655738ca7473ef697186db4ccb
patrik-ks17 <kasaip02@gmail.com>
2024-06-07 09:16:23 +0200

    unknown method error fixes

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Survey;
-use Illuminate\Http\Request;
 
 class SurveyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     public function create() {
         return view('survey.create');
     }
@@ -17,9 +22,10 @@ class SurveyController extends Controller
             'purpose' => 'required'
         ]);
 
-        $data['user_id'] = auth()->user()->id;
+    /*  $data['user_id'] = auth()->user()->id;
 
-        $survey = Survey::create($data);
+        $survey = Survey::create($data); */
+        $survey = auth()->user()->surveys()->create($data);
 
         return redirect('/survey/'. $survey->id);
     }
